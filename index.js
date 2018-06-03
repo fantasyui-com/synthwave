a81a59464d42baaf9f0f3918c6eb3f4a85970d2c
fantasyui-com <captain@fantasyui.com>
2018-06-03 12:33:55 -0400

    new array response.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const render = function (input){
   let list = Array.isArray(input)?input:[input];
   return list.map(function(i){
     const {id, type, angle, gradient} = i;
-    return `.${database.meta.name}-${id} { background: ${type}(${angle} ${gradient.map(i=>[i.color, i.position].join(" "))}) }`
-  }).join("\n");
+    return `.${database.meta.name}-${id} { background: ${type}(${angle}, ${gradient.map(i=>[i.color, i.position].join(" ") ).join(", ")}); }`
+  });
 }
 
 module.exports = {
     render,
     css: function(pattern){
       const selected = database.gradients.filter(gradient => gradient.id.match(pattern));
-      return selected.map(i => render(i));
+      return selected.map(i => render(i)).join("\n");
     },
     raw: function(pattern){
       const selected = database.gradients.filter(gradient => gradient.id.match(pattern));
