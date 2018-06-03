64649854b20a9f4921bb1a4a8135b0059e98b4e0
fantasyui-com <captain@fantasyui.com>
2018-06-03 12:09:20 -0400

    Enhanced API

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = require('fs').readFileSync(__dirname+'/style.css').toString();
+const fs = require('fs');
+const database = JSON.parse(fs.readFileSync(__dirname+'/style.json').toString());
+
+const render = function (input){
+  let list = Array.isArray(input)?input:[input];
+  return list.map(function(i){
+    const {id, type, angle, gradient} = i;
+    return `.${database.meta.name}-${id} { background: ${type}(${angle} ${gradient.map(i=>[i.color, i.position].join(" "))}) }`
+  }).join("\n");
+}
+
+module.exports = {
+    render,
+    css: function(pattern){
+      const selected = database.gradients.filter(gradient => gradient.id.match(pattern));
+      return selected.map(i => render(i));
+    },
+    raw: function(pattern){
+      const selected = database.gradients.filter(gradient => gradient.id.match(pattern));
+      return selected;
+    }
+};
