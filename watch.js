e527a941e2eae54420252d28bbf342c8473a6866
fantasyui-com <captain@fantasyui.com>
2018-06-04 13:59:01 -0400

    PostHTML and Watcher

--- watch.js
+++ watch.js
@@ -3,23 +3,32 @@ const path = require('path');
 const fs = require('fs');
 const posthtml = require('posthtml');
 
-function update(location){
+const minifier = require('posthtml-minifier');
 
-  console.log(location);
- 
+function update(source){
+
+  console.log(source);
+
   const result = posthtml()
   .use(require('posthtml-custom-elements')())
-  .process(fs.readFileSync(location), { sync: true })
+  .use(minifier({ collapseWhitespace: true, preserveLineBreaks: false, removeComments: true, minifyCSS: true, minifyJS: true }))
+  .process(fs.readFileSync(source), { sync: true })
   .html
 
-  console.log(result)
+  //console.log(result)
+  const sourcename = path.basename(source);
+  const basename = path.basename(source, '.src.html');
+  const dirname = path.dirname(source);
+  const destination = path.join(dirname , basename + '.html' )
+  fs.writeFileSync(destination, result + `\n\n <!-- view source: ${sourcename} -->\n`);
 
 }
 
-watch.watchTree(__dirname, {filter:function(location){ return path.extname(location) === '.html'}, ignoreDirectoryPattern:/node_modules/}, function (f, curr, prev) {
+watch.watchTree(__dirname, {filter: function(source){ return source.match(/\.src\.html$/) }, ignoreDirectoryPattern:/node_modules/}, function (f, curr, prev) {
   if (typeof f == "object" && prev === null && curr === null) {
     // Finished walking the tree
-    console.log(f)
+    //console.log(f)
+
   } else if (prev === null) {
 
     // f is a new file
